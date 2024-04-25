e1a4e464e0233835041ec5287a73aafe1f165c6f
Colin <colin.dubuis@protonmail.com>
2024-04-25 14:39:08 +0200

    birth of the watermelon class

---
+++ app/src/main/java/ch.zhaw.it.pm2.jvmjourney/GameEngine/WaterMelon.java
@@ -0,0 +1,12 @@
+package main.java.ch.zhaw.it.pm2.jvmjourney.GameEngine;
+
+public class WaterMelon extends ch.zhaw.it.pm2.jvmjourney.GameEngine.MovingObject {
+    public WaterMelon(int x, int y, String path) {
+        super(x, y, path);
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
